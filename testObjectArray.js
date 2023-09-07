7a747c263a5d3980cfb4aef4aeccd8cd4600f9bd
Varun Naik <Varunnaik2001@gmail.com>
2023-09-07 18:22:07 -0500

    added test data

---
+++ testObjectArray.js
@@ -0,0 +1,42 @@
+
+const testObjectArray = [
+    {
+      "id": "uniswap3",
+      "chain": "eth",
+      "name": "Uniswap V3",
+      "site_url": "https://app.uniswap.org",
+      "logo_url": "https://static.debank.com/image/project/logo_url/uniswap3/87a541b3b83b041c8d12119e5a0d19f0.png",
+      "has_supported_portfolio": true,
+      "tvl": 3743030990.973852,
+      "net_usd_value": 241.1066115236279,
+      "asset_usd_value": 241.1066115236279,
+      "debt_usd_value": 0
+    },
+    {
+      "id": "compound",
+      "chain": "eth",
+      "name": "Compound",
+      "site_url": "https://app.compound.finance",
+      "logo_url": "https://static.debank.com/image/project/logo_url/compound/0b792243f1f68e9ed082f5a49ee6f21d.png",
+      "has_supported_portfolio": true,
+      "tvl": 12763095483.420198,
+      "net_usd_value": 9.42968660318954,
+      "asset_usd_value": 10.610932251963174,
+      "debt_usd_value": 1.1812456487736345
+    },
+    {
+      "id": "curve",
+      "chain": "eth",
+      "name": "Curve",
+      "site_url": "https://curve.fi",
+      "logo_url": "https://static.debank.com/image/project/logo_url/curve/aa991be165e771cff87ae61e2a61ef68.png",
+      "has_supported_portfolio": true,
+      "tvl": 17053767979.356224,
+      "net_usd_value": 224.52344262613227,
+      "asset_usd_value": 224.52344262613227,
+      "debt_usd_value": 0
+    }
+    // more...
+  ]
+
+  export {testObjectArray};
