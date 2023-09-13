989c76d62c8a95fb68724ffe76d19563b4a9f196
Varun Naik <Varunnaik2001@gmail.com>
2023-09-12 23:01:28 -0500

    added logic to get addresses from file

--- fileCreator.js
+++ fileCreator.js
@@ -1,3 +1,10 @@
 import * as fs from 'fs';
 
+const getAddressesFromFile = function(filename,splitString){
+    const data = fs.readFileSync(filename,'utf8').split(splitString);
+    return data;
+
+}
+
+
 
