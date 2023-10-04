25f94a04e005aff849a48e53dff96bd8eae125b8
Varun Naik <Varunnaik2001@gmail.com>
2023-10-04 17:28:23 +0100

    style changes

--- apiCaller.js
+++ apiCaller.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// API KEY: 0ac87ffb582a30a47b4979d8ab704ae7c499929a
-
-
 var config = {
   method: 'get', 
   url: 'https://pro-openapi.debank.com/v1/user/all_complex_protocol_list',
@@ -19,7 +16,6 @@ var config = {
 const getPortfolioFromAddress =  async function(address) {
 
 config.params.id = address;
-
 let returnData = (await axios(config)).data;
 
 return returnData;
