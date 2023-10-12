242c1c248f5fee7f220cd0d3e43f2d3ec65c9e2b
Varun Naik <Varunnaik2001@gmail.com>
2023-10-12 19:44:38 +0100

    organized code by creating separate folder for helper functions

---
+++ helpers/apiCaller.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+var config = {
+  method: 'get', 
+  url: 'https://pro-openapi.debank.com/v1/user/all_complex_protocol_list',
+  headers: {
+    'accept': 'application/json',
+    'AccessKey': '0ac87ffb582a30a47b4979d8ab704ae7c499929a', 
+  },
+  params: {
+    id: '',
+    chain_ids: 'bsc,eth,matic,op,arb,avax,evmos,tlos,nova,boba,celo',
+  },
+};
+
+const getPortfolioFromAddress =  async function(address) {
+
+config.params.id = address;
+let returnData = (await axios(config)).data;
+
+return returnData;
+
+}
+
+export {getPortfolioFromAddress};
