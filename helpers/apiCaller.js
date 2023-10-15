648b3a7e933d3b739f676ffad602fa1658e56f9e
Varun Naik <Varunnaik2001@gmail.com>
2023-10-15 17:19:35 +0100

    added api call for Arkham

--- helpers/apiCaller.js
+++ helpers/apiCaller.js
@@ -1,25 +1,44 @@
 import axios from "axios";
 
-var config = {
-  method: 'get', 
-  url: 'https://pro-openapi.debank.com/v1/user/all_complex_protocol_list',
-  headers: {
-    'accept': 'application/json',
-    'AccessKey': '0ac87ffb582a30a47b4979d8ab704ae7c499929a', 
-  },
-  params: {
-    id: '',
-    chain_ids: 'bsc,eth,matic,op,arb,avax,evmos,tlos,nova,boba,celo',
-  },
-};
+
 
 const getPortfolioFromAddress =  async function(address) {
+  let config = {
+    method: 'get', 
+    url: 'https://pro-openapi.debank.com/v1/user/all_complex_protocol_list',
+    headers: {
+      'accept': 'application/json',
+      'AccessKey': '0ac87ffb582a30a47b4979d8ab704ae7c499929a', 
+    },
+    params: {
+      id: '',
+      chain_ids: 'bsc,eth,matic,op,arb,avax,evmos,tlos,nova,boba,celo',
+    },
+  };
 
 config.params.id = address;
-let returnData = (await axios(config)).data;
+const returnData = (await axios(config)).data;
 
 return returnData;
 
 }
 
-export {getPortfolioFromAddress};
+const queryArkham = async function(base_,flow_,limit_,usdGte_,timeLast_){
+  const config = {
+    method: "get",
+    url: "https://api.arkhamintelligence.com/transfers",
+    params: {
+      base: base_,
+      flow: flow_,
+      limit: limit_,
+      usdGte: usdGte_,
+      timeLast: timeLast_,
+    },
+  }
+  const returnData = (await axios(config)).data.transfers;
+
+  return returnData;
+
+}
+
+export {getPortfolioFromAddress,queryArkham};
