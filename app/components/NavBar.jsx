fa5c77ff5aeca91f7aaa8155f7220d013f266635
Varun Naik <Varunnaik2001@gmail.com>
2023-11-12 21:43:31 +0000

    added a navigation bar

---
+++ app/components/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Link from "next/link";
+//import Logo from "./Logo";
+import { Button } from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <>
+      <div class="w-full h-20 bg-emerald-800 sticky top-0">
+        <div class="container mx-auto px-4 h-full">
+          <div class="flex justify-between items-center h-full">
+            
+            <ul class="hidden md:flex gap-x-6 text-white">
+              <li>
+                <Link href="/about">
+                  <p>About Us</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/services">
+                  <p>Services</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contacts">
+                  <p>Contacts</p>
+                </Link>
+              </li>
+            </ul>
+            <Button />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export {Navbar};
+
+//<Logo />
