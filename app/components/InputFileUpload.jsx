43410da103f1c129259dc0ed4ce4e72228e6431a
Varun Naik <Varunnaik2001@gmail.com>
2023-11-15 14:27:00 +0000

    added button to propagate firestore with .csv upload

--- app/components/InputFileUpload.jsx
+++ app/components/InputFileUpload.jsx
@@ -1,23 +1,24 @@
-import { useState } from "react";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import { Container } from "@mui/material";
+import { useState } from "react";
+import { propagateFirestore } from "../..";
 
 const InputFileUpload = function() {
 
-
+  const [addressArray, setAddressArray] = useState([]);
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
     reader.readAsText(file);
     reader.onloadend = () => {
-      console.log( reader.result.split(','));
+      setAddressArray( reader.result.split(','));
     };
 
   };
 
   return (
-    <Container maxWidth="md" sx={{ mt: 8 }}>
+    <Container maxWidth="lg" sx={{ mt: 1 }}>
       <Stack direction="row" alignItems="center" spacing={2}>
         <label htmlFor="upload-file">
           <Button variant="contained" component="span">
@@ -32,6 +33,14 @@ const InputFileUpload = function() {
           />
         </label>
       </Stack>
+      <Button
+        variant= "contained"
+        onClick={()=>{
+          if(confirm(`Are you sure you want to create a dataset of the portfolios of ${addressArray.length} addresses?`) == true)
+          propagateFirestore(addressArray);
+        }}>
+        Run Debank 
+      </Button>
     </Container>
 
     );
